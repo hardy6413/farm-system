98dba379e647c1927cb9ba4353f7a6bf4781cea2
hardy6413 <irreftw@gmail.com>
2023-03-04 11:19:33 +0100

    initial setup

---
+++ src/main/java/com/example/farmsystem/FarmSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.farmsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/farmsystem/FarmSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.farmsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
